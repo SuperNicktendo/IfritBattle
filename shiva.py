596ffb4dbfee121f27b1a59ce48846b44edb7fb2
Nick Murray <106425332+SuperNicktendo@users.noreply.github.com>
2022-06-19 16:49:20 +0100

    Update shiva.py

--- shiva.py
+++ shiva.py
@@ -3,24 +3,24 @@ materia2 = "Blizzard"
 materia3 = "Thunder"
 
 print("One of your summons has gone rogue! You better tame them with the right materia!");
-materia = input("It's Ifrit! Will you use Fire, Blizzard or Thunder materia? ")
-if materia == materia2:
-    print("Excellent! Ifrit's flame is snuffed out by Blizzard!")
-elif materia == materia1:
-    print("Uh-oh! Fire has made Ifrit stronger!")
+materia = input("It's Shiva! Will you use Fire, Blizzard or Thunder materia? ")
+if materia == materia1:
+    print("Excellent! Shiva's power melts in the face of your Fire magic!")
+elif materia == materia2:
+    print("Uh-oh! Blizzard has made Shiva stronger!")
     materia = input("Try another materia: ")
-    if materia == materia2:
-        print("Excellent! Ifrit's flame is snuffed out by Blizzard!")
-    elif materia == materia1:
-        print("Ifrit's strength goes supernova! Your party falls...")
+    if materia == materia1:
+        print("Excellent! Shiva's power melts in the face of your Fire magic!")
+    elif materia == materia2:
+        print("You have empowered Shiva and she unleashes Diamond Dust! Your party falls...")
     elif materia == materia3:
-        print("Thunder has little effect on Ifrit, but it stuns him long enough for your party to flee!")
+        print("Thunder has little effect on Shiva, but it stuns her long enough for your party to flee!")
 elif materia == materia3:
-    print("You zap Ifrit with Thunder, but he shrugs it off.")
+    print("You zap Shiva with Thunder, but she shrugs it off.")
     materia=input("Try another materia: ")
-    if materia == materia2:
-        print("Excellent! Ifrit's flame is snuffed out by Blizzard!")
-    elif materia == materia1:
-        print("Ifrit's strength goes supernova! Your party falls...")
+    if materia == materia1:
+        print("Excellent! Shiva's power melts in the face of your Fire magic!")
+    elif materia == materia2:
+        print("You have empowered Shiva and she unleashes Diamond Dust! Your party falls...")
     elif materia == materia3:
         print("Your second Thunder attack stuns Ifrit, giving your party a chance to flee!")
